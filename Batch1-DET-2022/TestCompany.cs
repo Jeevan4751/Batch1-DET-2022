d410628cc542a299d3516753cba455a326c00d68
githubjeevan <105343415+githubjeevan@users.noreply.github.com>
2022-08-29 16:58:52 +0530

    Add files via upload

---
+++ Batch1-DET-2022/TestCompany.cs
@@ -0,0 +1,22 @@
+﻿using Batch1_DET_2022;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class TestCompany
+{
+        public static void Main()
+        {
+            
+            CompanyManager mgrn = new CompanyManager { name = "jeevan", surename = "s", phone = "8970006296" };
+            Company company = new Company { name = "sonata software", address = "global village", website = "ssl.com", faxnumber = "485", phone = "9844608892", Manager = mgrn };
+
+            Console.WriteLine(company.Print());
+            
+        }
+}
+}
